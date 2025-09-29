d013dc2f3225b6e52f8d914d9fc4032c46669dd6
Colby E <colby.endres@tamu.edu>
2025-09-29 09:56:49 -0500

    Frontend for paper add functionality

--- routes.py
+++ routes.py
@@ -1,24 +1,47 @@
-from flask import Blueprint, render_template, request , current_app, flash, get_flashed_messages
+import flask
 from requests import HTTPError
 
-bp = Blueprint('main', __name__)
+bp = flask.Blueprint('main', __name__)
 
 @bp.route("/", methods=["GET"])
 def home():
-    paper_estimate = current_app.recommender.get_total_papers()
-    return render_template("home.html", num_papers = paper_estimate)
+    paper_estimate = flask.current_app.recommender.get_total_papers()
+    return flask.render_template("home.html", num_papers = paper_estimate)
 
 @bp.route("/search", methods=["GET"])
 def search():
-    return render_template("search.html")
+    return flask.render_template("search.html")
 
 @bp.route("/results", methods=["POST"])
 def results():
-    query_title = request.form["title"]
-    k = int(request.form["k"])
+    query_title = flask.request.form["title"]
+    k = int(flask.request.form["k"])
     try:
-        recommendations = current_app.recommender.recommend(query_title, k)
-        return render_template("results.html", query=query_title, recommendations=recommendations)
+        recommendations = flask.current_app.recommender.recommend(query_title, k)
+        return flask.render_template("results.html", query=query_title, recommendations=recommendations)
     except HTTPError as e:
-        flash(f'Unable to parse title (HTTP code {e.response.status_code})', 'error')
-        return render_template("search.html")
+        flask.flash(f'Unable to parse title (HTTP code {e.response.status_code})', 'error')
+        return flask.render_template("search.html")
+    
+@bp.route("/publish", methods=["GET", "POST"])
+def publish():
+    if flask.request.method == 'GET':
+        return flask.render_template("publish.html")
+    else:
+        # Prioritize arXiv ID, if it exists
+        try:
+            arxiv_id = flask.request.form['arxiv_id']
+            if arxiv_id:
+                flask.current_app.recommender.add_by_id(arxiv_id)
+            else:
+                title = flask.request.form['title']
+                abstract = flask.request.form['abstract']
+                if not title or not abstract:
+                    flask.flash('Must supply both title and abstract', 'error')
+                else:
+                    flask.current_app.recommender.add_by_title(title, abstract)
+            flask.flash('Paper successfully added')
+        except (ValueError, TypeError) as e:
+            flask.flash(e, 'error')
+    return flask.render_template('publish.html') 
+                
